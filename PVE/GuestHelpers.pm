7919c4820c9b62ad9e869f4b9d72e11c90a42b74
Wolfgang Bumiller <w.bumiller@proxmox.com>
2017-06-09 09:55:58 +0200

    GuestHelpers: allow overriding $lockdir in test environments

--- PVE/GuestHelpers.pm
+++ PVE/GuestHelpers.pm
@@ -8,11 +8,12 @@ use PVE::Tools;
 # We use a separate lock to block migration while a replication job
 # is running.
 
+our $lockdir = '/var/lock/pve-manager';
+
 sub guest_migration_lock {
     my ($vmid, $timeout, $func, @param) = @_;
 
     my $lockid = "pve-migrate-$vmid";
-    my $lockdir = "/var/lock/pve-manager";
 
     mkdir $lockdir;
 
