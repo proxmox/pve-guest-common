proxmox/pve-guest-common

commit daf8fca57a34417365c873ed91f3a52bf0002a4f

refactor config_with_pending_array
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

PVE/GuestHelpers.pm
@@ -201,40 +201,43 @@ sub format_pending {
 }
 
 # returns the config as an array of hashes, each hash can have the following keys:
-# key (the config property name, non-optional)
-# value (the current value in effect - if any)
-# pending (a new, still pending, value - if any)
-# delete (when deletions are pending, this is set to either 2 (force) or 1 (graceful))
+#  key: the config property name, non-optional
+#  value: the current value in effect - if any
+#  pending: a new, still pending, value - if any
+#  delete: when deletions are pending, this is set to either 2 (force) or 1 (graceful)
 sub config_with_pending_array {
     my ($conf, $pending_delete_hash) = @_;
 
-    my $res = [];
+    my $pending = delete $conf->{pending};
+    # we don't care for snapshots in pending and it makes our loops throw up
+    delete $conf->{snapshots};
 
+    my $res = [];
     foreach my $opt (keys %$conf) {
-	next if ref($conf->{$opt});
-	my $item = { key => $opt };
-	$item->{value} = $conf->{$opt} if defined($conf->{$opt});
-	$item->{pending} = $conf->{pending}->{$opt} if defined($conf->{pending}->{$opt});
-	$item->{delete} = ($pending_delete_hash->{$opt}->{force} ? 2 : 1) if exists $pending_delete_hash->{$opt};
+	my $item = {
+	    key => $opt,
+	    value => $conf->{$opt},
+	};
+	$item->{pending} = delete $pending->{$opt} if defined($conf->{pending}->{$opt});
+	my $delete = delete $pending_delete_hash->{$opt};
+	$item->{delete} = $delete->{force} ? 2 : 1 if defined($delete);
 
 	push @$res, $item;
     }
 
-    foreach my $opt (keys %{$conf->{pending}}) {
+    foreach my $opt (keys %$pending) {
 	next if $opt eq 'delete';
-	next if ref($conf->{pending}->{$opt}); # just to be sure
-	next if defined($conf->{$opt});
-	my $item = { key => $opt };
-	$item->{pending} = $conf->{pending}->{$opt};
-
-	push @$res, $item;
+	push @$res, {
+	    key => $opt,
+	    pending => $pending->{$opt},
+	};
     }
 
     while (my ($opt, $force) = each %$pending_delete_hash) {
-	next if defined($conf->{pending}->{$opt});
-	next if defined($conf->{$opt});
-	my $item = { key => $opt, delete => ($force ? 2 : 1)};
-	push @$res, $item;
+	push @$res, {
+	    key => $opt,
+	    delete => $force ? 2 : 1,
+	};
     }
 
     return $res;

PVE/AbstractConfig.pm
@@ -82,7 +82,6 @@ sub parse_pending_delete {
     my $pending_deletions = {};
     for my $entry (split(/\s+/, $data)) {
 	my ($force, $key) = $entry =~ /^(!?)(.*)$/;
-
 	$pending_deletions->{$key} = {
 	    force => $force ? 1 : 0,
 	};

commit 301b375babf1acf4a434b94d7419bdd5f7d1dec7

unbreak config_with_pending_array
which lead to current and pending/delete values being returned
separately, and being misinterpreted by the web interface (and probably
other clients as well).

Fixes: daf8fca57a34417365c873ed91f3a52bf0002a4f

Signed-off-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>

PVE/GuestHelpers.pm
@@ -220,7 +220,7 @@ sub config_with_pending_array {
 	    key => $opt,
 	    value => $conf->{$opt},
 	};
-	$item->{pending} = delete $pending->{$opt} if defined($conf->{pending}->{$opt});
+	$item->{pending} = delete $pending->{$opt} if defined($pending->{$opt});
 	my $delete = delete $pending_delete_hash->{$opt};
 	$item->{delete} = $delete->{force} ? 2 : 1 if defined($delete);
 