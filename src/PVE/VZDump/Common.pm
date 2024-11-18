proxmox/pve-guest-common

commit 5118facafd863c86ccea1db1917c200372baac5d

vzdump: change-detection: fix description and drop experimental note
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/VZDump/Common.pm
@@ -356,8 +356,7 @@ my $confdesc = {
     },
     'pbs-change-detection-mode' => {
 	type => 'string',
-	description => "PBS mode used to detect file changes and switch encoding. NOTE: `data` and `metadata` modes are experimental."
-	    . " format for container backups.",
+	description => "PBS mode used to detect file changes and switch encoding format for container backups."
 	optional => 1,
 	enum => [ 'legacy', 'data', 'metadata' ],
     },

commit 9d15995df3b1b59ae673a80f770e2b4d2595a4ee

vzdump common: fix missing comma
Fixes: 5118fac ("vzdump: change-detection: fix description and drop experimental note")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/VZDump/Common.pm
@@ -356,7 +356,7 @@ my $confdesc = {
     },
     'pbs-change-detection-mode' => {
 	type => 'string',
-	description => "PBS mode used to detect file changes and switch encoding format for container backups."
+	description => "PBS mode used to detect file changes and switch encoding format for container backups.",
 	optional => 1,
 	enum => [ 'legacy', 'data', 'metadata' ],
     },