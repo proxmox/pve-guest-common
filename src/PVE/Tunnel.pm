proxmox/pve-guest-common

commit 8c8aa6319e2f257b04c2d2749b0845f4502ba771

tunnel: redirect stderr to log function
With open2(), the stderr from the child process would just be printed
directly making it impossible for the call site of the tunnel to
filter the output.

This is in preparation to add a 'pct mtunnel' command. When forking a
tunnel to a node which does not support that command yet, the whole
'pct' usage documentation would be printed to the migration log and
the call site would only see 'can't open tunnel - no reply' as an
error. By capturing the stderr output too, the call site can detect
that the failure happens because the target node is too old and avoid
dumping the whole usage information.

Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>
Reviewed-by: Daniel Kral <d.kral@proxmox.com>
Tested-by: Daniel Kral <d.kral@proxmox.com>
Link: https://lore.proxmox.com/all/20260225151931.176335-10-f.ebner@proxmox.com

src/PVE/Tunnel.pm
@@ -5,7 +5,7 @@ use warnings;
 
 use IO::File;
 use IO::Pipe;
-use IPC::Open2;
+use IPC::Open3 qw(open3);
 use JSON qw(encode_json decode_json);
 use POSIX qw( WNOHANG );
 use Storable qw(dclone);
@@ -66,12 +66,19 @@ sub read_tunnel {
     $timeout = 60 if !defined($timeout);
 
     my $reader = $tunnel->{reader};
+    my $reader_stderr = $tunnel->{reader_stderr};
 
     my $output;
     eval {
         PVE::Tools::run_with_timeout($timeout, sub { $output = <$reader>; });
     };
-    die "reading from tunnel failed: $@\n" if $@;
+    my $err = $@;
+
+    while (my $line = <$reader_stderr>) { # $reader_stderr is set up as non-blocking
+        $tunnel->{log}->('warn', $line);
+    }
+
+    die "reading from tunnel failed: $err\n" if $err;
 
     chomp $output if defined($output);
 
@@ -145,13 +152,16 @@ sub fork_ssh_tunnel {
     my $full_cmd = [@$rem_ssh, '-o ExitOnForwardFailure=yes', @localtunnelinfo, @$cmd];
 
     my $reader = IO::File->new();
+    my $reader_stderr = IO::File->new();
     my $writer = IO::File->new();
 
     my $orig_pid = $$;
 
     my $cpid;
 
-    eval { $cpid = open2($reader, $writer, @$full_cmd); };
+    eval { $cpid = open3($writer, $reader, $reader_stderr, @$full_cmd); };
+    # Only read_tunnel() uses $reader_stderr and it never wants to wait.
+    $reader_stderr->blocking(0);
 
     my $err = $@;
 
@@ -167,6 +177,7 @@ sub fork_ssh_tunnel {
     my $tunnel = {
         writer => $writer,
         reader => $reader,
+        reader_stderr => $reader_stderr,
         pid => $cpid,
         rem_ssh => $rem_ssh,
         log => $log,

commit e10fabc1f26d73767aa984adb329a5a67b6ea57e

tunnel: treat reader_stderr as optional parameter in read_tunnel
The reader_stderr parameter was introduced for a specific use case for
the users of fork_ssh_tunnel(). The other callers, such as
fork_websocket_tunnel(), do not provide the parameter and the Perl
interpreter will warn about this:

    Use of uninitialized value $reader_stderr in <HANDLE> at /usr/share/perl5/PVE/Tunnel.pm line 77.
    readline() on unopened filehandle at /usr/share/perl5/PVE/Tunnel.pm line 77.

Therefore, treat reader_stderr as an optional parameter by not reading
from the handle if it is not provided.

Fixes: 8c8aa63 ("tunnel: redirect stderr to log function")
Signed-off-by: Daniel Kral <d.kral@proxmox.com>
Tested-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Reviewed-by: Stoiko Ivanov <s.ivanov@proxmox.com>
Link: https://lore.proxmox.com/20260630084332.32566-1-d.kral@proxmox.com
[FE: add comment mentioning reader_stderr is only used by SSH tunnel]
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/Tunnel.pm
@@ -74,8 +74,10 @@ sub read_tunnel {
     };
     my $err = $@;
 
-    while (my $line = <$reader_stderr>) { # $reader_stderr is set up as non-blocking
-        $tunnel->{log}->('warn', $line);
+    if (defined($reader_stderr)) { # Only used by the ssh tunnel, but not by the websocket tunnel.
+        while (my $line = <$reader_stderr>) { # $reader_stderr is set up as non-blocking
+            $tunnel->{log}->('warn', $line);
+        }
     }
 
     die "reading from tunnel failed: $err\n" if $err;